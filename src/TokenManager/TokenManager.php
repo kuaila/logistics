915f42b24e660502eaa0ca3de4a5a632b6ba49ff
dingran <dingran80s@gmail.com>
2020-01-04 14:59:42 +0800

    获取token

---
+++ src/TokenManager/TokenManager.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dingran
+ * Date: 2020/1/4
+ * Time: 下午2:37
+ */
+
+namespace Kuailian\TokenManager;
+
+
+class TokenManager
+{
+    private $codeUrl = '';
+    private $tokenUrl = '';
+    private $refreshUrl = '';
+
+    public function getCodeUrl()
+    {
+        $this->codeUrl = $config['codeUrl'];
+    }
+
+    public function getTokenUrl()
+    {
+        $this->tokenUrl = $config['tokenUrl'];
+    }
+
+    public function getRefreshUrl()
+    {
+         $this->refreshUrl = $config['refreshUrl'];
+    }
+}
---
+++ src/config.php
@@ -0,0 +1,8 @@
+<?php
+$config = [
+    'codeUrl' => 'https://openapi-release.chukou1.cn/oauth2/authorization',
+    'tokenUrl' => 'https://openapi-release.chukou1.cn/oauth2/token',
+    'refreshUrl' => 'https://openapi-release.chukou1.cn'
+];
+
+return $config;
